b53adbd8ccad1e65d09d512014dd198f74a7cdaf
RL-JIT <ramlal@justit.co.uk>
2024-08-06 11:14:57 +0100

    Uploaded Cleaned Titanic Data and Script for Cleaning It

---
+++ 10 R Studio Data Analysis/Titanic_Data_Cleaning_Script.R
@@ -0,0 +1,26 @@
+# Installing and Loading Necessary Packages
+install.packages("dplyr")
+library(dplyr)
+# Reading CSV into a dataframe 
+titanic_data <- read.csv("Titanic-Dataset.csv")
+# Looking at dataframe in spreadsheet view
+View(titanic_data)
+# run summary stats on dataframe
+summary(titanic_data)
+# look at structure of dataframe
+str(titanic_data)
+
+# Fill missings values for Age with the median
+titanic_data <- titanic_data %>% mutate(Age = ifelse(is.na(Age), median(Age, na.rm = TRUE), Age)) 
+
+# Remove rows with missing Embarked Values
+titanic_data <- titanic_data %>% filter(!is.na(Embarked))
+
+# Drop the Cabin Column
+titanic_data <- titanic_data %>% select(-Cabin)
+
+# Convert relevant columns to appropriate data types
+titanic_data <- titanic_data %>% mutate(Survived = as.factor(Survived), Pclass = as.factor(Pclass), Sex = as.factor(Sex), Embarked = as.factor(Embarked)) 
+
+# Save my cleaned data to a new file
+write.csv(titanic_data, "cleaned_titanic_data.csv", row.names = FALSE)
